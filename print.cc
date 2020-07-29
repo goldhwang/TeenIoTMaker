37b1460ee324363d34ec6e2c1b0ef1305e9d0433
goldhwang <68631230+goldhwang@users.noreply.github.com>
2020-07-29 15:38:27 +0900

    Create print.cc
    황구찌해커역사1급 처음 깃허브 사용

---
+++ print.cc
@@ -0,0 +1,26 @@
+
+ //number나의 위치 값
+ int numX = 2020;
+ char s = 'A';
+
+ int numY = 1999;
+
+ int numA = 180;
+
+ char T = 'Y';
+
+ int numQ = 2050;
+ 
+void setup(){
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  Serial.println(numX + numY + numA);     // "Hi roblox."
+  delay(1000);
+  Serial.println(s);     // "Hi roblox."
+  delay(1000);
+  Serial.println(T);
+  delay(1000);
+  Serial.println(numQ);
+}
