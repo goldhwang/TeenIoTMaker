563926d277bab1a832c2c3f1a584890352d483e3
goldhwang <68631230+goldhwang@users.noreply.github.com>
2020-08-22 14:26:52 +0900

    Update IoTeeMaker0819.ino

--- IoTeeMaker0819/IoTeeMaker0819.ino
+++ IoTeeMaker0819/IoTeeMaker0819.ino
@@ -3,12 +3,28 @@
 2020 공개SW 개발자 대회 주용성, 황찬우 학생 참여중 입니다.
 이메일: goldhwang08@naver.com
 */
+
+
 #include <Arduino.h>
 #include <SPI.h>
 #include <WiFi101.h>
+
+
 #include "IoTMakers.h"
 #include "Shield_Wrapper.h"
-
+/*
+Dht11 sensor
+*/
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTPIN            5         // Pin which is connected to the DHT sensor.
+// Uncomment the type of sensor in use:
+//#define DHTTYPE           DHT11     // DHT 11 
+#define DHTTYPE           DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE           DHT21     // DHT 21 (AM2301)
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
 
 /*
 Arduino Shield
@@ -26,8 +42,8 @@ void init_shield()
 {
 	sdcard_deselect();
 	
-	const char* WIFI_SSID = "당신의 정보로 바꾸세요";
-	const char* WIFI_PASS = "당신의 정보로 바꾸세요";
+	const char* WIFI_SSID = "여러분의 개인정보를 넣어주세요";
+	const char* WIFI_PASS = "여러분의 개인정보를 넣어주세요";
 	g_shield.begin(WIFI_SSID, WIFI_PASS);
 
 	g_shield.print();
@@ -39,10 +55,9 @@ IoTMakers
 */
 IoTMakers g_im;
 
-const char deviceID[]   = "당신의 정보로 바꾸세요";
-const char authnRqtNo[] = "당신의 정보로 바꾸세요";
-const char extrSysID[]  = "당신의 정보로 바꾸세요";
-
+const char deviceID[]   = "여러분의 개인정보를 넣어주세요";
+const char authnRqtNo[] = "여러분의 개인정보를 넣어주세요";
+const char extrSysID[]  = "여러분의 개인정보를 넣어주세요";
 void init_iotmakers()
 {
 	Client* client = g_shield.getClient();
@@ -71,7 +86,7 @@ void init_iotmakers()
 		while(1);
 	}
 
-	//Serial.print(F("FreeRAM="));Serial.println(g_im.getFreeRAM());
+//	Serial.print(F("FreeRAM="));Serial.println(g_im.getFreeRAM());
 }
 
 #define PIN_LED		6
@@ -102,17 +117,16 @@ void loop()
 	if ( ( millis() - tick) > 3000 )
 	{
 		digitalWrite(PIN_LED, HIGH);
-		send_temperature();
-		send_light();
+		//send_temperature();//온도 보내기
+    send_temperature2();
+		send_light();//조도 보내기
 		digitalWrite(PIN_LED, LOW);
 
 		tick = millis();
  	}
-  
-	// IoTMakers 서버 수신처리 및 keepalive 송신
+  // IoTMakers 서버 수신처리 및 keepalive 송신
 	g_im.loop();
 }
-
 
 int send_temperature()
 {
@@ -127,6 +141,23 @@ int send_temperature()
 	}
 	return 0;   
 }
+//찬우 온도 보내기
+int send_temperature2()
+{
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+   int t = event.temperature;
+
+ //Serial.print(F("Temperature (c): ")); Serial.println(t);
+ if ( g_im.send_numdata("temperature", (double)t) < 0 ) {
+    //  Serial.println(F("fail"));  
+    return -1;
+  }
+  return 0;   
+}//찬우 온도 보내기 끝
 
 int send_light()
 {
